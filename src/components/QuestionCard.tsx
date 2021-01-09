ba108b1a660374e383d78ba362a03ed295a33051
Saad Shahid <saad.techgeek@gmail.com>
2021-01-09 16:53:58 +0500

    resolve type issue

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -18,7 +18,7 @@ const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer
         <div>
             {answers.map(answer => (
                 <div key={answer}>
-                    <button disabled={userAnswer} value={answer} onClick={callback}/>
+                    <button disabled={userAnswer?true:false} value={answer} onClick={callback}/>
                     <span dangerouslySetInnerHTML={{__html: answer}}></span>
                 </div>
             ))}
